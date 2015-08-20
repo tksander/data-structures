957fc436cc925e72477a11ffd6d21be507c441fc
tksander <tkepasander@gmail.com>
2015-08-19 21:58:12 -0700

    Complete prototypal queue and stack instantiation styles

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,42 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  // new queue var
+  var newQueue = Object.create(queueMethods);
+  // storage
+  newQueue.storage = {};
+  // current size
+  newQueue.currentSize = 0;
+  // head
+  newQueue.head = 0;
+  // current/"end"
+  newQueue.currentEnd = 0;
+
+  return newQueue;
 };
 
-var queueMethods = {};
+var queueMethods = {
+
+  // enqueue
+  enqueue: function(value) {
+    this.storage[this.currentEnd] = value;
+    this.currentSize++;
+    this.currentEnd++;
+  },
+  
+  // dequeue
+  dequeue: function() {
+    var dequeuedItem = this.storage[this.head];
+    delete this.storage[this.head];
+    this.head++;
+    if(this.currentSize > 0) {
+      this.currentSize--;
+    }
+    return dequeuedItem;
+  },
+
+  // size
+  size: function() {
+    return this.currentSize;
+  }
+};
 
 
--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,39 @@
+// Do:
+// Use Object.create with the object from step 2 to create instances of your class
+// Don't:
+// Use the keyword new
+
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  
+  // interface instance to pass back - use object.create
+  var newStack = Object.create(stackMethods);
+  // storage object
+  newStack.storage = {};
+  // counter
+  newStack.counter = 0; 
+  
+  return newStack;
 };
 
-var stackMethods = {};
+// use this to allow function instantiations to pass obj to interface
+var stackMethods = {
 
-
+  // push
+  push: function(value) {
+    this.storage[this.counter] = value;
+    this.counter++;
+  },
+  // pop
+  pop: function() {
+    var poppedItem = this.storage[this.counter-1];
+    delete this.storage[this.counter-1];
+    if (this.counter > 0) {
+      this.counter--;
+    }
+    return poppedItem;
+  },
+  // size
+  size: function() {
+    return this.counter;
+  }
+};
