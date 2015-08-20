fb008bf2ac26458559308b765ff6d9581d8dd821
tksander <tkepasander@gmail.com>
2015-08-20 10:43:15 -0700

    Complete pseudo-classical instatiation style

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,38 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  // storage
+  this.storage = {};
+  // counter/end
+  this.counter = 0;
+  // head
+  this.head = 0;
+  // current size
+  this.currentSize = 0;
 };
 
-
+Queue.prototype = {
+  // enqueue
+  enqueue: function(value) {
+    this.storage[this.counter] = value;
+    this.currentSize++;
+    this.counter++;
+  },
+  // dequeue
+  dequeue: function() {
+    var dequeuedItem = this.storage[this.head];
+    delete this.storage[this.head];
+    if (this.currentSize > 0) {
+      this.currentSize--;
+    }
+    this.head++;
+    return dequeuedItem;
+  },
+  // size
+  size: function() {
+    return this.currentSize;
+  },
+  // constructor
+  constructor: Queue
+};
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,41 @@
+// Do:
+// Work within the src/pseudoclassical/ folder
+// Capitalize your function name to indicate to others that it is intended to be run with the keyword new
+// Use the keyword new when instantiating your class
+// Use the keyword this in your constructor
+// Don't:
+// Declare the instance explicitly
+// Return the instance explicitly
+
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  // storage
+  this.storage = {};
+  // counter
+  this.counter = 0;  
 };
 
 
+Stack.prototype = {
+  // push
+  push: function(value) {
+    this.storage[this.counter] = value;
+    this.counter++;
+  }, 
+  // pop
+  pop: function() {
+    var poppedItem = this.storage[this.counter - 1];
+    delete this.storage[this.counter - 1];
+    if(this.counter > 0) {
+      this.counter--;
+    }
+    return poppedItem;
+  },
+  // size
+  size: function() {
+    return this.counter;
+  },
+
+ constructor: Stack
+};
+
+// Stack.prototype.constructor = Stack;  (Alternative way to add the constructor)
