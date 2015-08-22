5058ab83b660f5c5b14d2b69fd02d66ffdd96df3
tksander <tkepasander@gmail.com>
2015-08-22 16:40:56 -0700

    Refactor binary search tree

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -13,80 +13,63 @@ var BinarySearchTree = function(value){
 var bstMethods = {};
 
 bstMethods.insert = function(value) {
-  var initialNode = this;
-
   var childBST = BinarySearchTree(value);
 
-  var recurse = function(node) {
-
     // if less:
-    if (value < node.value) {
-      if (node.left === null) {  // if left is null:
+    if (value < this.value) {
+      if (this.left === null) {  // if left is null:
         // insert child into .left
-        node.left = childBST;
+        this.left = childBST;
       } else {  // if left is not null:
-        // recursively call recurse(), comparing "value" to value of left-hand node
-        recurse(node.left);
+        // recursively call this.insert(), comparing "value" to value of left-hand this
+        this.left.insert(value);
       }
 
     // if greater:
-    } else if (value > node.value) {
-      if (node.right === null) {  // if right is null:
+    } else if (value > this.value) {
+      if (this.right === null) {  // if right is null:
         // insert child into .right
-        node.right = childBST;
+        this.right = childBST;
       } else { // if right is not null:
-       // recursively call recurse(), comparing "value" to value of right-hand node
-       recurse(node.right);
+       // recursively call this.insert(), comparing "value" to value of right-hand this
+       this.right.insert(value);
       }
     }
-  };
-
-  recurse(initialNode);
 };
 
 bstMethods.contains = function(target) {
-  var initialNode = this;
   var found = false;
 
-  var recurse = function(node) {
-
-    if(target === node.value) {
-      found = true;
-    } else if (target < node.value) {
-        if (node.left !== null) {  
-          recurse(node.left);
-        }
-    } else if (target > node.value) {
-        if (node.right !== null) {  
-         recurse(node.right);
-        }
-    } 
-  };
-
-  recurse(initialNode);
+  if(target === this.value) {
+    found = true;
+  } else if (target < this.value) {
+      if (this.left !== null) {  
+       found = this.left.contains(target);
+      }
+  } else if (target > this.value) {
+      if (this.right !== null) {  
+       found = this.right.contains(target);
+      }
+  } 
   return found;
 };
 
 
-bstMethods.depthFirstLog = function(callback) {
-  var initialNode = this;
+bstMethods.depthFirstLog = function(callback, currentNode) {
+  currentNode = currentNode || this;
 
-  var recurse = function(node) {
-    // apply callback to current node value
-    callback(node.value);
+  // apply callback to current node value
+  callback(currentNode.value);
 
-    // if node.left not null, recurse over node.left
-    if (node.left !== null) {
-      recurse(node.left);
-    }
+  // if currentNode.left not null, recurse over currentNode.left
+  if (currentNode.left !== null) {
+    currentNode.depthFirstLog(callback, currentNode.left);
+  }
 
-    // if node.right not null, recurse over node.right
-    if (node.right !== null) {
-      recurse(node.right);
-    }
-  };
-
-  recurse(initialNode);
+  // if currentNode.right not null, recurse over currentNode.right
+  if (currentNode.right !== null) {
+    currentNode.depthFirstLog(callback, currentNode.right);
+  }
 };
 
 
