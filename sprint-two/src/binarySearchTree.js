a2ddc3049a02250bf13b7a25b773f70c641c975d
tksander <tkepasander@gmail.com>
2015-08-21 23:12:32 -0700

    Wrote up some initial code for BST

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,5 +1,29 @@
 var BinarySearchTree = function(value){
+
+   var newBST = {};
+
+  newBST.left = [];
+  newBST.right = [];
+
+  // add treemethods functionality to newBST
+  _.extend(newBST, bstMethods);
+  return newBST;
 };
+
+var bstMethods = {};
+
+bstMethods.insert = function(value) {
+
+  // if value < current node
+
+  // if value > node
+
+};
+bstMethods.contains = function() {};
+bstMethods.left = function() {};
+bstMethods.depthFirstLog = function() {};
+
+
 
 
 /*
